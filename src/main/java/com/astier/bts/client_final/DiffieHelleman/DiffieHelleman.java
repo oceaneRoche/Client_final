10da451021e7d93c4e15a96e88c6d18758afb213
oroche <oceane.roche@lyceeastier.com>
2024-09-23 15:24:23 +0200

    init

---
+++ src/main/java/com/astier/bts/client_final/DiffieHelleman/DiffieHelleman.java
@@ -0,0 +1,40 @@
+package com.astier.bts.client_final.DiffieHelleman;
+
+import com.astier.bts.client_final.tcp.TCP;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class DiffieHelleman {
+    TCP tcp = new TCP();
+    int taille = 0;
+    byte [] byte_B = new byte[65535];
+    BigInteger serveurB, a, A, k, p, g;
+
+    public DiffieHelleman(TCP tcp, int taille) {
+        this.tcp = tcp;
+        this.taille = taille;
+        a = new BigInteger(taille,new SecureRandom());
+        p = new BigInteger(taille,new SecureRandom());
+        do {
+            g = new BigInteger(taille,new SecureRandom());
+        }while (g.compareTo(p) > 0);
+    }
+    public byte [] recuperationParam(){
+        try {
+            tcp.outBin.write(p.toByteArray());
+
+        tcp.outBin.write(g.toByteArray());
+        A = g.modPow(a, p);
+        tcp.outBin.write(A.toByteArray());
+        int taille_b = tcp.inBin.read(byte_B);
+        serveurB = new BigInteger(Arrays.copyOfRange(byte_B, 0, taille_b));
+        k = serveurB.modPow(a, p);
+        } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+        return k.toByteArray();
+    }
+}
